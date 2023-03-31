fad7563519fd964d2a660be4bf07844470c3e828
kernaltrap <94473358+JamesIsWack@users.noreply.github.com>
2023-03-31 14:55:29 -0500

    Add files via upload

--- discord_moyai.py
+++ discord_moyai.py
@@ -5,11 +5,13 @@ import asyncio
 # to add the bot, simply paste "https://discord.com/api/oauth2/authorize?client_id=844572613421301761&permissions=0&scope=bot" into web browser, and add to your discord server
 
 # variables for bot usage
-TOKEN = 'ODQ0NTcyNjEzNDIxMzAxNzYx.GyxMMx.-7LBagkFq96-FYrLgdMSWNMjaFH3IsatonEs-s'
+TOKEN = 'bot_token_here'
 EMOJI = '🗿'
 TEXT = 'funy sound effec'
 CREATOR = 'i was created by JamesIsWack // kernaltrap'
 CREATOR_link = 'https://github.com/JamesIsWack'
+MENTION = 'bro i cant answer you i am literally only for sending the moyai emoji, talk to my creator instead'
+SRC = "https://github.com/JamesIsWack/moyai_discord_bot"
 HELP = '!spam // send the moyai 10 times.\n !creator // credits lol\n !commands // display this help message.'
 
 intents = discord.Intents().all() # probably shouldnt do this for security
@@ -56,10 +58,20 @@ async def on_message(message):
 @client.event
 async def on_message(message):
     if client.user.mentioned_in(message):
-        await message.channel.send('bro i cant answer you i am literally only for sending the moyai emoji, talk to my creator instead') # print this message
+        await message.channel.send(MENTION) # print this message
         await asyncio.sleep(0.8) # sleep 8th of a second
         await message.channel.send(CREATOR_link) # send creator link
         await asyncio.sleep(0.1) # sleep half a second
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('!src'):
+        for i in range(1):
+            await message.channel.send(SRC)
+            await asyncio.sleep(0.1) # sleep 0.1s
 
 client.run(TOKEN)
 
